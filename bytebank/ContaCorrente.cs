e676b6b49d1ae849b8defd99b72282e5fc142f98
Ramon Vicente Silva <vicente.silva.ramon@gmail.com>
2023-01-23 22:25:09 -0300

    Aula de classes

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public class ContaCorrente
+    {
+        public int numero_agencia;
+        public string conta;
+        public string titular;
+        public double saldo;
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (valor <= this.saldo)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,25 @@
+﻿using bytebank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "Andre Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-x";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine($"Saldo da conta do Andre = R${contaDoAndre.saldo}");
+
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine($"Saldo da conta do Andre pós-deposito = R${contaDoAndre.saldo}");
+
+if (contaDoAndre.Sacar(300) == true)
+{
+    Console.WriteLine($"Saldo da conta do Andre pós-saque = R${contaDoAndre.saldo}");
+}
+else
+{
+    Console.WriteLine("Tem grana não meu fi!!");
+}
+
+
