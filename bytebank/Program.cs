c98309758f3a1e5adbe9fcebaa8d32c44a83996a
Ramon Vicente <vicente.silva.ramon@gmail.com>
2023-05-10 14:19:17 -0300

    Trabalhando com arrays e coleções

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -9,107 +9,117 @@ using System.Net.Http.Headers;
 using System.Numerics;
 
 
-#region
-CalcularBonificacao();
-UsarSistema();
+Console.WriteLine("Bem vindo ao Byte bank");
 
-void CalcularBonificacao()
+//TestaArray();
+//TestaBuscarPalavra();
+void TestaArray()
 {
-    GerenciadorDeBonificacao gerenciador = new GerenciadorDeBonificacao();
+    int idade = 30;
 
-    Designer ulisses = new Designer("12589654256", "Ulises Peixoto");
+    int[] idades = new int[5];
 
-    Diretor paula = new Diretor("56986532515", "Paula Silveira");
+    idades[0] = 5;
+    idades[1] = 65;
+    idades[2] = 3;
+    idades[3] = 7;
+    idades[4] = 8;
 
-    Auxiliar igor = new Auxiliar("56898653654", "Igor Meses");
+    Console.WriteLine($"Tamanho do Array {idades.Length}"); 
 
-    GerenteDeContas camila = new GerenteDeContas("26956325698", "Camila furtis");
-
-    gerenciador.Registrar(ulisses);
-    gerenciador.Registrar(paula);
-    gerenciador.Registrar(igor);
-    gerenciador.Registrar(camila);
-
-    Console.WriteLine($"Total de bonificações {gerenciador.TotalDeBonificacao}");
+    for(int i = 0; i < idades.Length; i++)
+    {
+        Console.WriteLine($"Indice[{i}] idades {idades[i]}");
+    };
 
 }
 
-void UsarSistema()
+
+void TestaBuscarPalavra()
 {
-    SistemaInterno sistema = new SistemaInterno();
+    string[] arrayDePalavras = new string[5];
+    
+    for(int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite a {i+1}ª palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
 
-    Diretor ramon = new Diretor("2563265214", "Ramon Menezes");
-    ramon.Senha = "256";
+    }
 
-    GerenteDeContas Cassia = new GerenteDeContas("5632665896", "Cássia Martineira");
-    Cassia.Senha = "234";
+    Console.WriteLine("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
 
-
-    ParceiroComercial caio = new();
-    caio.Senha = "888";
-
-    sistema.Logar(ramon, "256");
-    sistema.Logar(Cassia, "456");
-    sistema.Logar(caio, "888");
-}
-
-Cliente joaozin = new("Joãozin da Beterraba", "25698654755", "Plantador");
-
-Console.WriteLine(joaozin.ToString());
-
-#endregion
-
-#region
-Cliente ramon = new Cliente("Ramon", "25632658955", "Vendedor de calendario");
-Cliente pereira = new Cliente("pereira", "2563256955", "Vendedor de calendario");
-
-Console.WriteLine(ContaCorrente.TaxaOperacao);
-
-
-
-try
-{
-    ContaCorrente conta1 = new ContaCorrente(ramon, 256, "1865-8");
-    ContaCorrente conta2 = new ContaCorrente(pereira, 125, "1825-8");
-    conta1.Sacar(50);
-    Console.WriteLine(conta1.GetSaldo());
-    conta1.Sacar(150);
-    Console.WriteLine(conta1.GetSaldo());
-
-}
-catch (ArgumentException ex)
-{
-    Console.WriteLine($"Parametro errado {ex.ParamName}");
-    Console.WriteLine("Não é possivel criar uma conta com numero de agência menor ou igual a zero.");
-    Console.WriteLine(ex.Message);
-}
-catch (SaldoInsuficienteException ex)
-{
-    Console.WriteLine($"Operação negada! Saldo insuficiente!");
-    Console.WriteLine(ex.Message);
-}
-
-#endregion
-
-
-
-LeitorDeArquivo leitor = new LeitorDeArquivo("contas.txt");
-
-try
-{
-    leitor.LerProximaLinha();
-    leitor.Dispose();
-;
-}
-catch(IOException)
-{
-    Console.WriteLine("Olha o erro ai!");
-}
-finally
-{
-    leitor.Dispose();
+    foreach (var palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine("ACHOU!");
+            Console.WriteLine($"Encontramos a palavra{palavra} que esta na posição {palavra}");
+        }
+    }
 }
 
 
+Array amostra = Array.CreateInstance(typeof(double), 5);
+
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array vazio");
+    }
+
+    double[] numerosOrdenados = (double[])array.Clone();
+    Array.Sort(numerosOrdenados);
+
+    int tamanho = numerosOrdenados.Length;
+    int meio = numerosOrdenados.Length / 2;
+
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"mediana igual {mediana}");
+}
 
 
+//TestaMediana(amostra);
+
+
+
+Array Vl = Array.CreateInstance(typeof(double), 9);
+
+for (int i = 0; i < Vl.Length; i++)
+{
+    Console.WriteLine($"Insira o {i + 1}ª valor: ");
+    double input = Convert.ToDouble(Console.ReadLine());
+    Vl.SetValue(input, i);
+}
+
+
+void CaMedia(Array array)
+{
+    var total = 0.0;
+
+    if (array.Length == 0 || array == null)
+    {
+        Console.WriteLine("Sem valor");
+        return;
+    }
+
+    foreach (var value in array)
+    {
+        Console.WriteLine(value);
+        total = total + (double)value;
+    }
+
+    double metade = total / array.Length;
+
+    Console.WriteLine($"A media do array é {metade}");
+}
+
+CaMedia(Vl);
